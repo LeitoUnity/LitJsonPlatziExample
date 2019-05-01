25fdec7c20d6c9339363204cb01779c7208275f6
Leo <leonardosolorzano16@gmail.com>
2019-05-01 19:56:16 -0300

    First Commit

---
+++ LitJsonExample/Assets/Scripts/ReadJson.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+public class ReadJson : MonoBehaviour
+{
+    private string jsonText;
+    private JsonData itemData;
+    private Weapon weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //jsonText = File.ReadAllText(Application.dataPath + "/Files/Items.json");
+
+        //itemData = JsonMapper.ToObject(jsonText);
+
+        //Debug.Log(itemData["Weapons"][1]["damage"]);
+
+        weapon = new Weapon("Pistola Platzi", 20);
+
+        itemData = JsonMapper.ToJson(weapon);
+
+        File.WriteAllText(Application.dataPath + "/Files/weapon.json", itemData.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Weapon
+{
+    public string Name;
+    public int Damage;
+
+    public Weapon(string Name = "DefaultName", int Damage = 10)
+    {
+        this.Name = Name;
+        this.Damage = Damage;
+    }
+}
